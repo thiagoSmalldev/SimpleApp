c843758a871352f2410f07ac9bce5fd949cb3699
Thiago Araujo <thiagoaraujo.dev@gmail.com>
2023-10-26 18:21:42 -0300

    create a helper to remove if from the view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_notification
+    if notice.present?
+      content_tag(:p, notice, class: "notice alert alert-success")
+    elsif alert.present?
+      content_tag(:p, alert, class: "alert alert-warning")
+    end
+  end
 end
